49a2dc7782aba081566044b5eb1f6998d8cef6a9
Gravermann <Sven.Gravermann@gws.ms>
2019-10-16 15:31:43 +0200

    Model

---
+++ turnschuh/turnschuh/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User 
+
+class TransferFile(models.Model):
+    path = models.FileField(upload_to="uploads/")
+    user = models.ForeignKey(User)
+    direction = models.BooleanField(choices=((True,"Upload"),(False,"Download")))
+    transferTime = models.DateTimeField(auto_now=True)
+    fileType = models.IntegerField(choices=((0,"TaskData"),(1,"Documents")), default=1)
