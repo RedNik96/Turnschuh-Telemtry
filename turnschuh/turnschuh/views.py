8ae3724621e095dc96fb03874575f56ee82f00d3
Florian <florian-hat@web.de>
2019-10-16 17:41:46 +0200

    Florian2

--- turnschuh/turnschuh/views.py
+++ turnschuh/turnschuh/views.py
@@ -1,8 +1,12 @@
+import os
 from django.http import HttpResponse
 from django.template import loader
+from django.conf import settings
+from wsgiref.util import FileWrapper
 
 
 def index(request):
+
     latest_question_list = [1,2,3,4]
     template = loader.get_template('turnschuh/index.html')
     context = {
@@ -11,5 +15,9 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 
-
-    
+    # generate the file
+    myfile= os.path.join(settings.MEDIA_ROOT,"uploads/dummy.zip")
+    print(myfile)
+    response = HttpResponse(FileWrapper(open(myfile,"rb")), content_type='text/plain')
+    response['Content-Disposition'] = 'attachment; filename=dummy'
+    return response
