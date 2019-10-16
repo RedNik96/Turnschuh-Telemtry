fec62cc9a912be303a09a0b3e2d9952150960248
rednik96 <rednik.develop@gmail.com>
2019-10-16 16:59:10 +0200

    adding simple login mechanism

--- turnschuh/turnschuh/urls.py
+++ turnschuh/turnschuh/urls.py
@@ -19,5 +19,6 @@ from turnschuh import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^login$', views.LoginView.as_view(), name='login')
 ]
--- turnschuh/turnschuh/views.py
+++ turnschuh/turnschuh/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth import authenticate, login
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
 from django.template import loader
+from django.views import View
 
 
 def index(request):
@@ -10,6 +14,21 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'turnschuh/login.html')
 
-
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect(reverse('index'))
+        else:
+            ctx = {
+                'error': True,
+                'username': username
+            }
+            return render(request, 'turnschuh/login.html', ctx)
     
