918b5b96881bf5dbc7e22bf33654ee1861d0bed2
Gravermann <Sven.Gravermann@gws.ms>
2019-10-16 13:06:57 +0200

    Database

--- turnschuh/turnschuh/settings.py
+++ turnschuh/turnschuh/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'turnschuh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'floriansql1',
+        'USERNAME': 'floriansql1',
+        'PASSWORD': 'farmhack',
+        'HOST': 'http://v22016053608234601.powersrv.de' 
     }
 }
 
